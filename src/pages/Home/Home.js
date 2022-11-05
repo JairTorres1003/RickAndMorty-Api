80f8c9247726340c3a962120a04bf769f4c914ac
Jair Torres <83931760+JairTorres1003@users.noreply.github.com>
2022-11-05 11:32:06 -0500

    Update Home.js

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -43,7 +43,7 @@ function Home() {
     
 }
 function HomeCharacters() {
-    window.location.href = window.location.href + 'characters'
+    alert('characters')
 }
 function HomeEpisodes() {
     alert('episodes')
